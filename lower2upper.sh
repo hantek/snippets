1d9cd05a674a347320df260254a3fcfe2413223a
Jonathan Zhouhan LIN <lin.zhouhan@gmail.com>
2017-11-20 17:42:34 -0500

    Create lower2upper.sh

---
+++ lower2upper.sh
@@ -0,0 +1,10 @@
+find . -depth | \
+    while read LONG 
+    do 
+        SHORT=$( basename "$LONG" | tr '[:lower:]' '[:upper:]' )
+        DIR=$( dirname "$LONG" ) 
+        if [ "${LONG}" != "${DIR}/${SHORT}"  ] 
+        then 
+            mv "${LONG}" "${DIR}/${SHORT}" 
+        fi 
+    done
